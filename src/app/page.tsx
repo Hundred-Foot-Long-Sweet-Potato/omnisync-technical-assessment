e968389b3793bea426f718fba4ebb644210154db
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2025-08-31 01:22:16 -0700

    Fixed an onLoad bug
    onLoad didn't preserve main number order after first sort. That and it didn't have the correct array for the oldArray since it wasn't copying the parameter array.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   useEffect(() => {
     const fetchOrInitializeCards = async () => {
       const res = await fetch('/api/cards');
-      const data: CardData[] = await res.json();
+      let data: CardData[] = await res.json();
 
       // Nothing in data means first time load
       if (data.length === 0) {
@@ -35,6 +35,7 @@ export default function Home() {
         }
       }else{
         // OnLoad sorts by first click time
+        data = data.sort((a, b) => a.mainNumber - b.mainNumber);
         sortByFirstClick(data,true);
       }
     }
@@ -94,7 +95,7 @@ export default function Home() {
   }
 
   const sortByFirstClick = (cards : CardData[], sortByFirst : boolean = true) => {
-    const oldArray = [...cardArray];
+    const oldArray = [...cards];
     const sorted = [...cards].sort((a, b) => {
       const aTime = a.timeOfFirstClick ? new Date(a.timeOfFirstClick).getTime() : (sortByFirst ? Infinity : -Infinity);
       const bTime = b.timeOfFirstClick ? new Date(b.timeOfFirstClick).getTime() : (sortByFirst ? Infinity : -Infinity);
@@ -147,7 +148,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center select-none">
       <main>
-        <button className=" absolute top-2 left-2 bg-white w-15 h-15 rounded-full hover:scale-105 hover:bg-gray-200 card border-black border-2 component" onClick={toggleLightMode}></button>
+        <button className=" absolute top-2 left-2 bg-white w-15 h-15 rounded-full hover:scale-105 hover:bg-gray-200 card border-black border-2 component " onClick={toggleLightMode}>toggle light</button>
         <h1 className="text-6xl font-bold pb-10"> Card Counter!</h1>
         <div className="grid grid-cols-4 grid-rows-2 gap-8">
           {cardArray.map((card) => (
@@ -155,15 +156,15 @@ export default function Home() {
           ))}
         </div>
         <div className="pt-10 flex justify-center gap-4 ">
-          <button className="h-10 w-20 bg-white text-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={resetCards}>Reset</button>
+          <button className="h-10 w-20 bg-white text-black border-2 border-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={resetCards}>Reset</button>
         </div>
         <div className="pt-10 flex justify-center gap-4 ">
-          <button className="h-10 w-40 bg-white text-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={() => sortByClicks(true)}>Sort by Most Clicks</button>
-          <button className="h-10 w-40 bg-white text-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={() => sortByFirstClick(cardArray,true)}>Sort by First Click</button>
+          <button className="h-10 w-40 bg-white text-black border-2 border-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={() => sortByClicks(true)}>Sort by Most Clicks</button>
+          <button className="h-10 w-40 bg-white text-black border-2 border-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={() => sortByFirstClick(cardArray,true)}>Sort by First Click</button>
         </div>
         <div className="pt-10 flex justify-center gap-4">
-          <button className="h-10 w-40 bg-white text-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={() => sortByClicks(false)}>Sort by Least Clicks</button>
-          <button className="h-10 w-40 bg-white text-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={() => sortByFirstClick(cardArray,false)}>Sort by Last Click</button>
+          <button className="h-10 w-40 bg-white text-black border-2 border-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={() => sortByClicks(false)}>Sort by Least Clicks</button>
+          <button className="h-10 w-40 bg-white text-black border-2 border-black rounded hover:scale-105 hover:bg-gray-200 component" onClick={() => sortByFirstClick(cardArray,false)}>Sort by Last Click</button>
         </div>
       </main>
     </div>
